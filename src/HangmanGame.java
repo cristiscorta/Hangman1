fcb2b85b18be544c560023f3859ba453e7a76b32
cristiscorta <cristi.scorta@yahoo.ro>
2018-12-04 21:11:14 +0200

    Initial commit

---
+++ src/HangmanGame.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HangmanGame {
+    public static final char PLACEHOLDER = '_';
+    public static final int MAX_TRIES = 6;
+    String word;
+    Integer triesLeft;
+    char[] guessedLetters;
+
+    public HangmanGame(String word) {
+        this.word = word;
+        this.triesLeft= MAX_TRIES;
+        guessedLetters = new char[word.length()];
+        Arrays.fill(guessedLetters, PLACEHOLDER);
+
+    }
+
+    public void play(){
+        while(triesLeft>0){
+            showGuessedLetters();
+            String letter=insertLetter();
+            if(!verifyLetter(letter)){
+                triesLeft--;
+                showTriesLeft();
+            }else{
+                if(guessedAllLetters()){
+                    System.out.println("AI GASTIGAT!");
+                    return;
+                }
+            }
+        }
+        System.out.println("AI PIERDUT!");
+    }
+    private String insertLetter(){
+        System.out.print("Introdu o litera: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+    private Boolean verifyLetter(String letter){
+        Integer index= word.indexOf(letter);
+        if(index>=0){
+            do{
+                guessedLetters[index]=letter.charAt(0);
+                index=word.indexOf(letter,index+1);
+            }while(index!=-1);
+            return true;
+        }
+        return false;
+
+    }
+    private void showGuessedLetters(){
+        for(int i=0;i<word.length();i++){
+            System.out.print(guessedLetters[i]+" ");
+        }
+        System.out.println();
+    }
+    private void showTriesLeft(){
+        System.out.println("Mai ai "+triesLeft+" incercari");
+    }
+    private Boolean guessedAllLetters(){
+       return new String(guessedLetters).indexOf(PLACEHOLDER)<0;
+    }
+}
